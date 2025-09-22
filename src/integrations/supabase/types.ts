cb63875c7906d2d93885d156954d892e3d0f3c42
Maarixt <omarifrancis36@gmail.com>
2025-09-22 12:33:25 -0700

    feat(alerts): allow configuring pin and level

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -141,11 +141,13 @@ export type Database = {
           id: string
           label: string
           max_value: number | null
+          message: string | null
           min_value: number | null
           override_mode: boolean | null
           pin: number | null
           state: Json | null
           type: string
+          trigger: number | null
           updated_at: string
         }
         Insert: {
@@ -157,11 +159,13 @@ export type Database = {
           id?: string
           label: string
           max_value?: number | null
+          message?: string | null
           min_value?: number | null
           override_mode?: boolean | null
           pin?: number | null
           state?: Json | null
           type: string
+          trigger?: number | null
           updated_at?: string
         }
         Update: {
@@ -173,11 +177,13 @@ export type Database = {
           id?: string
           label?: string
           max_value?: number | null
+          message?: string | null
           min_value?: number | null
           override_mode?: boolean | null
           pin?: number | null
           state?: Json | null
           type?: string
+          trigger?: number | null
           updated_at?: string
         }
         Relationships: [
